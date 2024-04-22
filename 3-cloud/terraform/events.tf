51548b5c31acedd69e2109d134a219a895da079e
Joe Mulvey <joe.mulvey@northcoders.com>
2024-04-22 10:53:09 +0100

    Clarify instructions for Terraform

--- 3-cloud/terraform/events.tf
+++ 3-cloud/terraform/events.tf
@@ -1,3 +1,4 @@
 resource "aws_cloudwatch_event_rule" "scheduler" {
- # to do
+ # to do - this should set up a scheduler that will trigger the Lambda every 5 minutes
+ # careful! other things may need to be set up as well
 }
