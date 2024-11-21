feed8dd240ce45f4bef7eba49e7c8cbfeaf7f900
nc-eli-wiggins <elijah.wiggins@northcoders.com>
2024-11-21 11:39:59 +0000

    add explanation of lambda layer and layer creation

--- 3-cloud/terraform/lambda.tf
+++ 3-cloud/terraform/lambda.tf
@@ -1,4 +1,3 @@
-/*
 data "archive_file" "lambda" {
   type             = "zip"
   output_file_mode = "0666"
@@ -6,10 +5,19 @@ data "archive_file" "lambda" {
   output_path      = "${path.module}/../function.zip"
 }
 
+
+/*
+The quotes.py lambda will need access to the dependencies in the layer folder to run the code in AWS, these dependencies are bundled into a .zip file and added to the s3 in s3.tf 
+*/
+
 data "archive_file" "layer" {
-  #TODO: Use this archive_file block to create a deployment package for the layer.
+  type = "zip"
+  output_file_mode = "0666"
+  source_dir = "${path.module}/../layer/"
+  output_path = "${path.module}/../layer.zip"
 }
 
+/*
 resource "aws_lambda_layer_version" "requests_layer" {
   layer_name          = "requests_layer"
   compatible_runtimes = [var.python_runtime]
@@ -22,3 +30,4 @@ resource "aws_lambda_function" "quote_handler" {
   #TODO: Connect the layer which is outlined above
 }
 */
+
